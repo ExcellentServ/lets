cde7efb231eba0de856f784b5d67239f937ed1e4
ExcellentServ <sharedup@gmail.com>
2015-01-04 20:38:10 +0200

    First commit

---
+++ settings.py
@@ -0,0 +1,36 @@
+from lino.projects.std.settings import *
+
+
+class Site(Site):
+
+    title = "Lino LETS Tutorial (1)"
+
+    #~ user_model = None
+
+    def setup_menu(self, ui, profile, main):
+        m = main.add_menu("master", "Master")
+        m.add_action(self.modules.lets.Products)
+        m.add_action(self.modules.lets.Customers)
+        m.add_action(self.modules.lets.Providers)
+
+        m = main.add_menu("market", "Market")
+        m.add_action(self.modules.lets.Offers)
+        m.add_action(self.modules.lets.Demands)
+
+        m = main.add_menu("config", "Configure")
+        #~ m.add_action('users.Users')
+        m.add_action(self.modules.lets.Places)
+
+
+#~ SITE = Site(globals(),'lino.modlib.users','lino.tutorials.lets1.lets')
+SITE = Site(globals(), 'lets')
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'lino2',
+	'USER': 'root',
+    }
+}
+DEBUG = True
+
